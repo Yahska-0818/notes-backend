f0b3efd2b63e1cfebfce7ed3aa9e7c154ee945d8
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-07-26 11:09:15 +0000

    added user information in notes and notes informations for users

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('notes', { content: 1, important: 1 })
   response.json(users)
 })
 
@@ -22,6 +22,11 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
+})
+
+usersRouter.get('/:id', async (request,response) => {
+  const user = await User.findById(request.params.id).populate('notes', { content: 1, important: 1 })
+  response.json(user)
 })
 
 module.exports = usersRouter
