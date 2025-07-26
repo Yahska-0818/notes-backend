cb11523e5b1e9c2767481c42203b4080aa8ca474
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-07-26 16:10:29 +0000

    added token based authentication and logins

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,7 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
@@ -19,17 +20,24 @@ notesRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
-    return response.status(400).json({ error: 'userId missing or not valid' })
-  }
-
-  if (!body.content) {
-    return response.status(400).json({ error: 'content missing' })
+    return response.status(400).json({ error: 'UserId missing or not valid' })
   }
 
   const note = new Note({
@@ -41,6 +49,7 @@ notesRouter.post('/', async (request, response) => {
   const savedNote = await note.save()
   user.notes = user.notes.concat(savedNote._id)
   await user.save()
+
   response.status(201).json(savedNote)
 })
 
