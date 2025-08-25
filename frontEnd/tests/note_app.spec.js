d4d2ff5e948bc2477ea79c29ba0cc05e21aa7f29
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-08-25 18:12:54 +0000

    api access of database to test

--- frontEnd/tests/note_app.spec.js
+++ frontEnd/tests/note_app.spec.js
@@ -1,8 +1,26 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Note app', () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
     await page.goto('http://localhost:5173')
+  })
+
+  test('login fails with wrong password', async ({ page }) => {
+    await page.getByRole('button', { name: 'log in' }).click()
+    const textboxes = await page.getByRole('textbox').all()
+    await textboxes[0].fill('mluukkai')
+    await textboxes[1].fill('wrong')
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('wrong credentials')).toBeVisible()
   })
 
   test('front page can be opened', async ({ page }) => {
@@ -33,6 +51,17 @@ test.describe('Note app', () => {
       await page.getByRole('textbox').fill('a note created by playwright')
       await page.getByRole('button', { name: 'save' }).click()
       await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
+    test.describe('and a note exists', () => {
+      test.beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'new note' }).click()
+        await page.getByRole('textbox').fill('another note by playwright')
+        await page.getByRole('button', { name: 'save' }).click()
+      })
+      test('importance can be changed', async ({ page }) => {
+        await page.getByRole('button', { name: 'make not important' }).click()
+        await expect(page.getByText('make important')).toBeVisible()
+      })
     })
   })
 })
